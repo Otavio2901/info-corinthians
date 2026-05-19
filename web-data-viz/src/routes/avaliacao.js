3f54ceda297581fee5f602a48dd7882ff10b6b20
Otavio2901 <otavio.lemos@sptech.school>
2026-05-18 21:27:32 -0300

    feat:add authentication and feedback routes

---
+++ web-data-viz/src/routes/avaliacao.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var router = express.Router();
+
+var usuarioController = require("../controllers/usuarioController");
+
+//Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
+router.post("/cadastrar", function (req, res) {
+    avaliacaoController.cadastrar(req, res);
+})
+
+router.post("/autenticar", function (req, res) {
+    avaliacaoController.autenticar(req, res);
+});
+
+module.exports = router;
